e4d5d229cd71ad3a478cead31e8f7482eaa3cb3b
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-25 16:39:17 +0100

    add scaffholding with separation admin and guests

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Auth::routes();
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index');
 });
+
+Route::get('{any?}', function () {
+    return view('guests.home');
+})->where('any', '.*');
